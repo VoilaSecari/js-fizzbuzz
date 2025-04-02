e56a7d5085281b8161223c66e02cd753bb035864
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-04-02 16:11:06 +0200

    codice completato

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 //multipli di 3 = Fizz
 //multipli di 5 = Buzz
 //multipli di 3 e 5 = FizzBuzz
+
+//partenza
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz " + i);
+  else if (i % 3 === 0) console.log("Fizz " + i);
+  else if (i % 5 === 0) console.log("Buzz " + i);
+  else console.log(i);
+}
